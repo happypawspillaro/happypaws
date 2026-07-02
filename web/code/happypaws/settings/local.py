23ca650e3521fda812102ba68143fd2c8210fbb6
José Jácome <josejacomeb@gmail.com>
2026-07-01 22:31:41 -0500

    tarea: settings producción y local - Unificar las configuraciones comunes en base.py - Utilizar un archivo local.py para manejar configuraciones por defecto como DB sqlite, modo debug y sin instrucciones HTTPS - Archivo prod.py para guardar las configuraciones de HTTPS, base de datos PostgreSQL - Deprecar el uso de environ porque esta gestionado por docker

---
+++ web/code/happypaws/settings/local.py
@@ -0,0 +1,31 @@
+# No comprobar en ciertas líneas F403 y F405 para compartir las configuraciones del base.py
+import os
+
+from .base import *  # noqa: F403
+
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # noqa: F405
+]
+
+# Python Debug Toolbar
+if DEBUG:
+    import socket
+
+    INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+    # Obtener IP de Docker
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips]
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": "debug_toolbar.middleware.show_toolbar_with_docker",
+    }
+
+# Usar SQLLite en modo desarrollo
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR.parent / "db" / "db.sqlite3",
+    }
+}
